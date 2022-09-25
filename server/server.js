b9fbfd46fb3fae3a763b169afc8562f9236ccbfd
LeeJaeLee2610 <cuonggaayf01@gmail.com>
2022-09-25 22:10:47 +0700

    Update 25/9/02022

--- server/server.js
+++ server/server.js
@@ -1,6 +1,27 @@
 import express from "express";
-import io from "socket.io";
+import http from "http";
+import bodyParser from "body-parser";
+import { Server } from "socket.io";
+import path from "path";
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const server = http.createServer(app);
+const io = new Server(server);
+const __dirname = path.resolve();
 
-app.listen(3030);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", socket => {
+  console.log("a user connected");
+  socket.on("chat message", msg => {
+    console.log("message: " + msg);
+  });
+});
+
+server.listen(3030, () => {
+  console.log("Port 3030");
+});
