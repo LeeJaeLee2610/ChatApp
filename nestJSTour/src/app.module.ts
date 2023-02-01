5dc4399351ac4fdbcbfb135dd9a1c672e8f4d3ec
LeeJaeLee2610 <cuonggaayf01@gmail.com>
2023-02-01 14:10:21 +0700

    Update 1/2/2023

--- nestJSTour/src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { CatsModule } from './cat/cats.module';
-import { Module } from '@nestjs/common';
-
-@Module({
-  imports: [CatsModule]
-})
-export class AppModule {}
--- nestJSTour/src/cat/cats.module.ts
+++
@@ -1,9 +0,0 @@
-import { CatsService } from './services/cats.service';
-import { CatsController } from './controllers/cats.controller';
-import { Module } from '@nestjs/common';
-
-@Module({
-  controllers: [CatsController],
-  providers: [CatsService],
-})
-export class CatsModule {}
